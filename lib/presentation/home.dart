60b789ef89987ecbac639c5197870f8222cd5dac
Lysenko Vladyslav <33177301+vladuhalys@users.noreply.github.com>
2023-03-09 12:22:19 +0200

    common comit #2: work with flags was done

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:dash_flags/dash_flags.dart';
+import 'package:country_codes/country_codes.dart';
+import 'package:blurrycontainer/blurrycontainer.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,13 +15,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFF8EA9FB),
+    return Scaffold(
+      backgroundColor: const Color(0xFF8EA9FB),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 80, 0, 0),
+        padding: const EdgeInsets.fromLTRB(20, 80, 0, 0),
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
                 Text(
                   "Get Started",
@@ -31,22 +35,72 @@ class _HomeState extends State<Home> {
                 ),
               ],
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Row(
-                children: [
-                ActionChip(
-                  label: Text('+380'),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 160, 0, 0),
+              child: Row(children: [
+                GestureDetector(
+                  onTap: () {
+                    //TODO
+                    // showMaterialModalBottomSheet(
+                    //   context: context,
+                    //   builder: (context) => SingleChildScrollView(
+                    //     controller: ModalScrollController.of(context),
+                    //     child: Container(),
+                    //   ),
+                    // );
+                  },
+                  child: BlurryContainer(
+                    blur: 2,
+                    height: 48,
+                    elevation: 0,
+                    borderRadius: const BorderRadius.all(Radius.circular(16)),
+                    color: const Color(0xFFB7C8FD),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 12),
+                          child: CountryFlag(
+                            country: Country.fromCode('ua'),
+                            height: 20,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(6, 0, 12, 0),
+                          child: Text(
+                            "+380",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                              color: Color(0xff594C74),
+                              fontSize: 16,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                InputChip(label: Text('(123)123-1234')),
                 //TODO
-              ]
-              ),
+                // InputChip(label: Text('(123)123-1234')),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: BlurryContainer(
+                    blur: 2,
+                    height: 48,
+                    width: 24,
+                    elevation: 0,
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    color: Colors.black,
+                    child: TextField()
+                  ),
+                ),
+              ]),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: const FloatingActionButton(
         backgroundColor: Colors.white,
         onPressed: null,
         child: Icon(
