e142568f74b22dc14f325c11bb1d90987e02816f
Lysenko Vladyslav <33177301+vladuhalys@users.noreply.github.com>
2023-03-11 23:47:41 +0200

    Create Model: Phone for provider context

---
+++ lib/domain/model/phone.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+
+class Phone with ChangeNotifier, DiagnosticableTreeMixin {
+  String? _phone;
+  String? _countryCode;
+  String get phone => (_phone == null)? "" : _phone!;
+  String? get countryCode => _countryCode;
+
+  void setCountryCode(String? countryCode) async {
+    _countryCode = countryCode;
+    notifyListeners();
+  }
+
+  void setPhoneNumber(String? phone) async {
+    _phone = phone;
+    notifyListeners();
+  }
+
+  int getPhoneStringSize() {
+    return (phone != null) ? phone.length : 0;
+  }
+  String getFullPhone() {
+    return countryCode!+phone;
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) async {
+    super.debugFillProperties(properties);
+    properties.add(StringProperty('phone', phone));
+    properties.add(StringProperty('countryCode', countryCode));
+  }
+}
