8a99d07e7d08b945e752a49c50cd4f507e1c892b
Lysenko Vladyslav <33177301+vladuhalys@users.noreply.github.com>
2023-03-12 20:39:22 +0200

    Update file: chage widgets structure.

--- lib/presentation/widgets/home_modal_bottom_sheet.dart
+++ lib/presentation/widgets/home_modal_bottom_sheet.dart
@@ -1,9 +1,10 @@
-import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:flutter/material.dart';
+
+import 'home_modal_bottom/modal_app_bar.dart';
+import 'home_modal_bottom/modal_country_list.dart';
 
 class ModalScreen extends StatefulWidget {
   const ModalScreen({super.key});
-
   @override
   State<ModalScreen> createState() => _ModalScreenState();
 }
@@ -11,30 +12,14 @@ class ModalScreen extends StatefulWidget {
 class _ModalScreenState extends State<ModalScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      height: 500,
-      child: Column(
-        children: [
-          Align(
-            alignment: Alignment.topRight,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(0, 20, 20, 0),
-              child: BlurryContainer(
-                  blur: 2,
-                  height: 20,
-                  width: 20,
-                  elevation: 0,
-                  borderRadius: BorderRadius.all(Radius.circular(6)),
-                  color: Color(0xFFB7C8FD),
-                  child: Icon(
-                    //TODO new widget
-                    Icons.close_rounded,
-                    size: 12,
-                  )),
-            ),
-          ),
-        ],
-      ),
+    return const Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: ModalAppBar(),
+        ),
+        ModalCountryList(),
+      ],
     );
   }
 }
